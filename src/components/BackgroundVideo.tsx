fff0dc83e9ff59e66329f7a1d182c3664853c927
Ross Maguire <ross.rmdesigns@gmail.com>
2021-11-17 23:18:48 +0000

    fix: :bug: fixing typo in Videobackground scss module

--- src/components/BackgroundVideo.tsx
+++ src/components/BackgroundVideo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "scss/components/VideoBackGround.module.scss";
+import styles from "scss/components/VideoBackground.module.scss";
 
 interface IBackgroundVideoProps {
   source: string;

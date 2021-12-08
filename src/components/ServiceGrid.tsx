716b683ae7578929329ce626f99fa65d4430713c
Ross Maguire <ross.rmdesigns@gmail.com>
2021-12-08 19:34:49 +0000

    feat: :memo: updating URLs in ServiceGrid component to go to relevant pages

--- src/components/ServiceGrid.tsx
+++ src/components/ServiceGrid.tsx
@@ -17,28 +17,31 @@ function ServiceGrid() {
             We believe in a sustainable future, our products are fully
             recyclable and also made from recycled material.{" "}
           </p>
-          <Button buttonText="Learn more" buttonURL="#" />
+          <Button buttonText="Learn more" buttonURL="/posts/eco-friendly" />
         </div>
         <div className={styles.feature}>
           <img
             src="/images/service-grid/customer-photos.jpg"
-            alt="Eco Friendly"
+            alt="Customer Photos"
           />
           <h3>Customer Photos</h3>
           <p>
             Have a look at some pictures from our happy customers if you need
             ideas on how to use or decorate our cardboard creations.
           </p>
-          <Button buttonText="See more" buttonURL="#" />
+          <Button buttonText="See more" buttonURL="/customer-photos" />
         </div>
         <div className={styles.feature}>
-          <img src="/images/service-grid/faqs-image.webp" alt="Eco Friendly" />
+          <img
+            src="/images/service-grid/faqs-image.webp"
+            alt="Have Questions?"
+          />
           <h3>Have Questions?</h3>
           <p>
             Still wondering what Cardboard Creations are doing and how? Find
             more about our products by reading what others have been asking.
           </p>
-          <Button buttonText="Read more" buttonURL="#" />
+          <Button buttonText="Read more" buttonURL="/faqs" />
         </div>
       </div>
     </div>

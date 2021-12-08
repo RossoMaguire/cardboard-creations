06e889371720e62c167745da0293ef03400bc41f
Ross Maguire <ross.rmdesigns@gmail.com>
2021-12-08 19:34:08 +0000

    feat: :zap: adding customer photos page
    Grabbing media items from the api and showing them as customer photos in a grid on the new page

---
+++ src/pages/customer-photos.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
+import { getNextStaticProps } from "@faustjs/next";
+import { client } from "client";
+import { Footer, Header, Hero } from "components";
+import { GetStaticPropsContext } from "next";
+import Head from "next/head";
+import styles from "scss/pages/customer-photos.module.scss";
+
+export default function Page() {
+  const { useQuery } = client;
+  const generalSettings = useQuery().generalSettings;
+  const photos = useQuery().mediaItems().nodes;
+
+  return (
+    <>
+      <Header description={generalSettings.description} />
+
+      <Head>
+        <title>Customer Photos - {generalSettings.title}</title>
+      </Head>
+
+      <Hero title="Customer Photos" />
+
+      <main className="content content-single">
+        <div className="wrap">
+          <div className={styles.customerPhotos}>
+            {photos.map((photo) => (
+              <div
+                key={`customer-photo-${photo.id}`}
+                className={styles.customerPhoto}
+              >
+                <img key={photo.id} src={photo.guid} alt="Customer Photo" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+
+      <Footer copyrightHolder={generalSettings.title} />
+    </>
+  );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return getNextStaticProps(context, {
+    Page,
+    client,
+  });
+}

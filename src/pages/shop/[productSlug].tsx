3c272dbdf534deead53542c823f717608faf223e
Ross Maguire <ross.rmdesigns@gmail.com>
2021-12-11 21:37:19 +0000

    fix: :recycle: make individual product pages SSR to pickup WP product postings

--- src/pages/shop/[productSlug].tsx
+++ src/pages/shop/[productSlug].tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
-import { getNextStaticProps } from "@faustjs/next";
+import { getNextServerSideProps, getNextStaticProps } from "@faustjs/next";
 import axios from "axios";
 import _ from "lodash";
 import { client } from "client";
 import { Footer, Header, Hero, ProductDetail } from "components";
-import { GetStaticPropsContext } from "next";
+import { GetServerSidePropsContext, GetStaticPropsContext } from "next";
 import Head from "next/head";
 import React from "react";
 
@@ -45,34 +45,33 @@ export default function Page({ products = null, pageUri }) {
   );
 }
 
-export async function getStaticProps(context: GetStaticPropsContext) {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   const { data: products } = await axios.get(
     process.env.BASE_URL + "/api/woocommerce/products"
   );
 
-  return getNextStaticProps(context, {
+  return getNextServerSideProps(context, {
     Page,
     client,
     props: {
       products,
       pageUri: context.params.productSlug,
     },
-    revalidate: 10,
   });
 }
 
-export async function getStaticPaths() {
-  // Call an external API endpoint to get products
-  const { data: products } = await axios.get(
-    process.env.BASE_URL + "/api/woocommerce/products"
-  );
+// export async function getStaticPaths() {
+//   // Call an external API endpoint to get products
+//   const { data: products } = await axios.get(
+//     process.env.BASE_URL + "/api/woocommerce/products"
+//   );
 
-  // Get the paths we want to pre-render based on products
-  const paths = products.products.map((product) => ({
-    params: { productSlug: product.slug },
-  }));
+//   // Get the paths we want to pre-render based on products
+//   const paths = products.products.map((product) => ({
+//     params: { productSlug: product.slug },
+//   }));
 
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
-}
+//   // We'll pre-render only these paths at build time.
+//   // { fallback: false } means other routes should 404.
+//   return { paths, fallback: false };
+// }

7f7f561956c8e85fe0ad5e1db12d9673ceee8701
Ross Maguire <ross.rmdesigns@gmail.com>
2021-12-13 15:37:57 +0000

    test: :test_tube: for the ProductDetail component

---
+++ src/components/Products/__tests__/ProductDetail.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+
+import ProductDetail from "../ProductDetail";
+import { mockProduct } from "../../common/mock_data";
+
+describe("Rendering the Product", () => {
+  it("Renders the product details correcty", async () => {
+    render(<ProductDetail product={mockProduct} />);
+
+    expect(screen.getByText("Home Bar - Cardboard Bar")).toBeInTheDocument();
+    expect(screen.queryByText("Our Best Seller")).not.toBeInTheDocument();
+  });
+
+  it("Renders the product as Best Seller", async () => {
+    render(<ProductDetail product={mockProduct} isBestSeller />);
+
+    expect(screen.getByText("Our Best Seller")).toBeInTheDocument();
+  });
+});
